402491497f765a8a7e360ca2fbb9bff64109a841
Vincent Traag <vincent@traag.net>
2021-10-12 14:51:43 +0200

    Changed datasource to MAG.

--- sql/data.sql
+++ sql/data.sql
@@ -1,15 +1,19 @@
-SELECT STRING_AGG(author, ';') AS authors, 
-    source, pub.pub_year, pub.n_cits, n_refs, title, abstract
-  FROM wos_2113..pub
-  INNER JOIN wos_2113..pub_detail
-    ON pub_detail.ut = pub.ut
-  INNER JOIN wos_2113..pub_abstract
-    ON pub_abstract.ut = pub.ut
-  INNER JOIN wos_2113_classification..clustering
-    ON clustering.ut = pub.ut
-  INNER JOIN wos_2113..pub_author
-    ON pub_author.ut = pub.ut
-  INNER JOIN wos_2113..author 
-    ON author.author_id = pub_author.author_id
-  WHERE cluster_id1 = 116
-  GROUP BY source, pub.pub_year, pub.n_cits, n_refs, title, abstract
+SELECT
+  authors.authors, journal_title = journal.display_name, pub_year = paper.[year], 
+  n_cits = paper.citation_count, n_refs = paper.reference_count, 
+  paper.paper_title, paper_abstract.abstract
+FROM mag_2019dec_classification..clustering
+JOIN mag_2019dec..paper
+  ON paper.paper_id = clustering.paper_id
+LEFT JOIN mag_2019dec..paper_abstract
+  ON paper_abstract.paper_id = paper.paper_id
+LEFT JOIN mag_2019dec..journal
+  ON paper.journal_id = journal.journal_id
+LEFT JOIN (SELECT a.paper_id, authors = STRING_AGG(b.display_name, '; ')
+           FROM mag_2019dec..paper_author AS a
+           INNER JOIN mag_2019dec..author AS b
+             ON a.author_id = b.author_id
+           GROUP BY a.paper_id) AS authors
+  ON authors.paper_id = paper.paper_id
+WHERE cluster_id1 = 198
+ORDER BY pub_year, journal_title, paper_title, authors
